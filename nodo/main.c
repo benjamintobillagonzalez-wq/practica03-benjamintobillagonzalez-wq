d3e66afa4a7313739a22e7ed842029735515993e
BenjaminTobilla <benjamintobillagonzalez@gmail.com>
2026-03-31 09:58:08 -0600

    Update nodo main
    Implemented node creation, value assignment, and destruction.

--- nodo/main.c
+++ nodo/main.c
@@ -5,26 +5,30 @@ int main()
 {
     printf("Prueba del TAD Nodo\n");
 
-    /* TODO
-       1 Crear un nodo con valor 10
-    */
-    Nodo* n = NULL;
+    /* 1 Crear un nodo con valor 10 */
+    Nodo* n = crearNodo(10);
 
-    /* TODO
-       2 Mostrar el valor del nodo usando obtenerValor
-    */
 
-    /* TODO
-       3 Cambiar el valor a 25 usando asignarValor
-    */
+    /* 2 Mostrar el valor del nodo usando obtenerValor */
+    if (n != NULL) {
+        printf("Valor inicial del nodo: %d\n", obtenerValor(n));
+    }
 
-    /* TODO
-       4 Mostrar nuevamente el valor
-    */
 
-    /* TODO
-       5 Liberar el nodo con destruirNodo
-    */
+    /* 3 Cambiar el valor a 25 usando asignarValor */
+    asignarValor(n, 25);
+    printf("Cambiando valor a 25...\n");
+
+
+    /* 4 Mostrar nuevamente el valor */
+    if (n != NULL) {
+        printf("Nuevo valor del nodo: %d\n", obtenerValor(n));
+    }
+
+
+    /* 5 Liberar el nodo con destruirNodo */
+    destruirNodo(n);
+    printf("Nodo destruido correctamente.\n");
 
     return 0;
 }
